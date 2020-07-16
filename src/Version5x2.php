cfe7e1735b906e0a22cc8d9a73de3a7e02a24fdf
Jeff Sturek <jsturek8@unl.edu>
2020-07-16 14:45:07 -0500

    Initial 5.2 setup

---
+++ src/Version5x2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Base class for Version 5.2 (2020) template files.
+ *
+ * @category  Templates
+ * @package   UNL_Templates
+ * @author    Jeff Sturek <jsturek8@unl.edu>
+ * @copyright 2020 Regents of the University of Nebraska
+ * @license   http://wdn.unl.edu/software-license BSD License
+ */
+
+namespace UNL\Templates;
+
+abstract class Version5x2 extends Templates
+{
+    const IS_CUSTOM = false;
+    const VERSION = '5.2';
+    const LOCAL_NAME = 'Version5x2';
+    const SOURCE_ROOT = 'https://raw.githubusercontent.com/unl/wdntemplates/5.2/';
+    const INCLUDE_ROOT = '/wdn/templates_5.2/includes/';
+}
